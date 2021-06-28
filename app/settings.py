25d07062ec2b0c96977a51d9ad9ebee03edc3f4f
Kazon Wilson <49080981+AlphaTheW0lf@users.noreply.github.com>
2021-06-27 20:52:11 -0500

    Adding configurations and DB models

---
+++ app/settings.py
@@ -0,0 +1,12 @@
+from starlette.config import Config
+from starlette.datastructures import CommaSeparatedStrings
+
+config = Config(".env")
+
+DEBUG = config("DEBUG", cast=bool, default=False)
+DB_NAME = config("DATABASE_NAME")
+DB_USER = config("DATABASE_USER")
+DB_PASS = config("DATABASE_PASSWORD")
+DB_HOST = config("DATABASE_HOST", default="localhost")
+DB_PORT = config("DATABASE_PORT", cast=int, default=3306)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=CommaSeparatedStrings, default="127.0.0.1,localhost")
