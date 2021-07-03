fee33fc223c17f2c201b60cd63a7b8a36f437df7
Kazon Wilson <kazonwilson@gmail.com>
2021-07-02 21:12:49 -0500

    Add retailers API; Fix selenium issue in linux

--- app/settings.py
+++ app/settings.py
@@ -3,6 +3,7 @@ from shutil import which
 from starlette.config import Config
 from starlette.datastructures import CommaSeparatedStrings
 
+
 config = Config(".env")
 
 DEBUG = config("DEBUG", cast=bool, default=False)
@@ -15,22 +16,7 @@ HOST = config("HOST", default="localhost")
 PORT = config("PORT", cast=int, default=8000)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=CommaSeparatedStrings, default="127.0.0.1,localhost")
 APP_ENV = config("APP_ENV", default="development")
-SELENIUM_DRIVER_EXECUTABLE_PATH = config("SELENIUM_DRIVER_EXECUTABLE_PATH", default=which("chromedriver"))
-SELENIUM_DRIVER_ARGUMENTS = [
-    "--headless",
-    "--no-sandbox",
-    "--disable-dev-shm-usage",
-    "--window-size=1920,1080",
-    "--disable-extensions",
-    "--proxy-server='direct://'",
-    "--proxy-bypass-list=*",
-    "--start-maximized",
-    "--disable-gpu",
-    "--ignore-certificate-errors",
-    "--test-type",
-    "--no-first-run",
-    "--no-default-browser-check",
-]
+CHROME_DRIVER_EXECUTABLE_PATH = config("CHROME_DRIVER_EXECUTABLE_PATH", default=which("chromedriver"))
 CHROME_BINARY_LOCATION = which("google-chrome") or "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 REDIS_HOST = config("REDIS_HOST", default="localhost")
 REDIS_PORT = config("REDIS_PORT", cast=int, default=6379)
--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,8 @@
 # Instal Google Chrome
-sudo apt-get install libxss1 libappindicator1 libindicator7
+sudo apt-get -y install libxss1 libappindicator1 libindicator7 libxi6 libgconf-2-4
+sudo apt-get -y install xorg xvfb gtk2-engines-pixbuf
+sudo apt-get -y install dbus-x11 xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic xfonts-scalable
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 sudo dpkg -i google-chrome*.deb
@@ -7,13 +10,15 @@ sudo apt-get install -f -y
 
 # Install Chromedriver
 wget -N https://chromedriver.storage.googleapis.com/91.0.4472.101/chromedriver_linux64.zip
-sudo apt-get install unzip -y
+sudo apt-get -y install unzip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
 
 sudo mv -f chromedriver /usr/local/share/chromedriver
 sudo ln -sf /usr/local/share/chromedriver /usr/local/bin/chromedriver
 sudo ln -sf /usr/local/share/chromedriver /usr/bin/chromedriver
+
+sudo chown root:root /usr/bin/chromedriver
 
 # Clean up
 sudo rm chromedriver_linux64.zip google-chrome-stable_current_amd64.deb
