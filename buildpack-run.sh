9627008b918285287b2dd4482e5b1abc42529858
Kazon Wilson <10987837+kwilson21@users.noreply.github.com>
2021-07-02 22:06:09 -0500

    Remove sudo from build pack script (#15)
    * Make buildpack script executable
    
    * Remove sudo

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,24 +1,24 @@
 # Instal Google Chrome
-sudo apt-get -y install libxss1 libappindicator1 libindicator7 libxi6 libgconf-2-4
-sudo apt-get -y install xorg xvfb gtk2-engines-pixbuf
-sudo apt-get -y install dbus-x11 xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic xfonts-scalable
+apt-get -y install libxss1 libappindicator1 libindicator7 libxi6 libgconf-2-4
+apt-get -y install xorg xvfb gtk2-engines-pixbuf
+apt-get -y install dbus-x11 xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic xfonts-scalable
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-sudo dpkg -i google-chrome*.deb
-sudo apt-get install -f -y
+dpkg -i google-chrome*.deb
+apt-get install -f -y
 
 # Install Chromedriver
 wget -N https://chromedriver.storage.googleapis.com/91.0.4472.101/chromedriver_linux64.zip
-sudo apt-get -y install unzip
+apt-get -y install unzip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
 
-sudo mv -f chromedriver /usr/local/share/chromedriver
-sudo ln -sf /usr/local/share/chromedriver /usr/local/bin/chromedriver
-sudo ln -sf /usr/local/share/chromedriver /usr/bin/chromedriver
+mv -f chromedriver /usr/local/share/chromedriver
+ln -sf /usr/local/share/chromedriver /usr/local/bin/chromedriver
+ln -sf /usr/local/share/chromedriver /usr/bin/chromedriver
 
-sudo chown root:root /usr/bin/chromedriver
+chown root:root /usr/bin/chromedriver
 
 # Clean up
-sudo rm chromedriver_linux64.zip google-chrome-stable_current_amd64.deb
+rm chromedriver_linux64.zip google-chrome-stable_current_amd64.deb
