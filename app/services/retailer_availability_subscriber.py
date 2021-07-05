1ad978a556d6a70d44c18ef925f2f9f9814ad8ac
Kazon Wilson <kazonwilson@gmail.com>
2021-07-05 11:16:39 -0500

    Remove joinall call

--- app/services/retailer_availability_subscriber.py
+++ app/services/retailer_availability_subscriber.py
@@ -4,8 +4,7 @@ import gevent
 
 from app.constants import RETAILER_AVAILABILITY_REDIS_CHANNEL
 from app.constants import RETAILER_REDIS_CONN
-from app.db.models import User
-from app.models.ps5_version import PS5Version
+from app.db.models import ConsolePreference
 from app.models.retailer import Retailer as RetailerModel
 from app.services import notifications
 
@@ -19,29 +18,20 @@ def _retailer_availability_handler(message: str) -> None:
     phone_numbers: List[str] = []
     emails: List[str] = []
 
-    for retailer_info in User.retailer_info:
-        for console_preference in retailer_info.console_preferences:
-            if PS5Version[console_preference.ps5_version] in retailer.versions_in_stock:
-                if retailer_info.user.notify_by_sms:
-                    phone_numbers.append(retailer_info.user.phone_number)
-                if retailer_info.user.notify_by_email:
-                    emails.append(retailer_info.user.email)
-
-    sms_jobs: gevent.Greenlet = []
-    email_jobs: gevent.Greenlet = []
+    for console_preference in ConsolePreference.select().where(
+        ConsolePreference.ps5_version << [v.value for v in retailer.versions_in_stock]
+    ):
+        if console_preference.retailer_info.user.notify_by_sms:
+            phone_numbers.append(console_preference.retailer_info.user.phone_number)
+        if console_preference.retailer_info.user.notify_by_email:
+            emails.append(console_preference.retailer_info.user.email)
 
     while phone_numbers[:]:
         numbers_to_text, phone_numbers = phone_numbers[:300], phone_numbers[300:]
-        sms_jobs.append(gevent.spawn(notifications.sms_retailer_availabilities, retailer, numbers_to_text))
+        gevent.spawn(notifications.sms_retailer_availabilities, retailer, numbers_to_text)
 
     for email in emails:
-        email_jobs.append(gevent.spawn(notifications.email_retailer_availabilities, email))
-
-    for email_job in email_jobs:
-        email_job.join()
-
-    for sms_job in sms_jobs:
-        sms_job.join()
+        gevent.spawn(notifications.email_retailer_availabilities, email)
 
 
 subscriber = RETAILER_REDIS_CONN.pubsub()
