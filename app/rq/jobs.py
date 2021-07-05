98ce0e3bc767cbd3a058ed6edb4f824f45556e61
Kazon Wilson <10987837+kwilson21@users.noreply.github.com>
2021-07-05 14:03:15 -0500

    Modify retailer availabilities (#21)

--- app/rq/jobs.py
+++ app/rq/jobs.py
@@ -1,18 +1,14 @@
 from app.constants import RETAILER_AVAILABILITY_REDIS_CHANNEL
 from app.constants import RETAILER_REDIS_CONN
-from app.constants import TARGET_RETAILER
 from app.db import retailer_store
 from app.retailers.retailer_factory import RetailerFactory
 
 
-def update_target_retailer_availabilities() -> None:
-    target_retailer = RetailerFactory.get_retailer(TARGET_RETAILER)
+def update_retailer_availabilities(retailer_name: str) -> None:
+    retailer = RetailerFactory.get_retailer(retailer_name)
 
-    retailer_availabilities = target_retailer.get_retailer_availabilities()
+    retailer_availabilities = retailer.get_retailer_availabilities()
 
     retailer_store.update_retailer_availabilities(retailer_availabilities)
 
     RETAILER_REDIS_CONN.publish(RETAILER_AVAILABILITY_REDIS_CHANNEL, retailer_availabilities.to_json())  # type: ignore
-
-
-all_jobs = [update_target_retailer_availabilities]
--- app/rq/scheduler.py
+++ app/rq/scheduler.py
@@ -2,12 +2,15 @@ from datetime import datetime
 
 from rq_scheduler import Scheduler
 
+from app.constants import RETAILERS
 from app.constants import RQ_REDIS_CONN
-from app.rq.jobs import all_jobs
+from app.rq.jobs import update_retailer_availabilities
 
 scheduler = Scheduler(connection=RQ_REDIS_CONN)
 
 
 def schedule_periodic_jobs():
-    for job in all_jobs:
-        scheduler.schedule(scheduled_time=datetime.utcnow(), func=job, interval=1200)  # Run every 30 minutes
+    for retailer_name in RETAILERS:
+        scheduler.schedule(
+            scheduled_time=datetime.utcnow(), func=update_retailer_availabilities, args=[retailer_name], interval=1200
+        )  # Run every 30 minutes
