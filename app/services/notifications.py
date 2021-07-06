b1bd728e9cb3fefba3857d30a61899e622a8d734
Kazon Wilson <10987837+kwilson21@users.noreply.github.com>
2021-07-06 09:26:18 -0500

    fix sms notifications (#23)
    * fix sms notifications
    
    * update readme
    
    * Add extra new line to message
    
    * show output of unit test in workflow
    
    * Fix twillio settings missing
    
    * add twilio number to env variable

--- app/services/notifications.py
+++ app/services/notifications.py
@@ -1,35 +1,31 @@
 from typing import List
 
-import requests  # type: ignore
+from twilio.rest import Client
 
 from app import settings
 from app.models.retailer import Retailer as RetailerModel
 
+client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
-def sms_retailer_availabilities(retailer: RetailerModel, numbers_to_text: List[str]) -> None:
-    text_str = f"{retailer.name} has PS5 versions in stock!"
+
+def sms_retailer_availabilities(retailer: RetailerModel, phone_number: str) -> None:
+    text_str = f"{retailer.name} has PS5 versions in stock!\n"
     responses: List[str] = []
 
     for availability in retailer.in_stock_availabilities:
-        text_str += f"\nVERSION: {availability.version.value} PRICE: {availability.price}"
+        text_str += f"\nVersion: {availability.version.value}"
+        text_str += " " * 6
+        text_str += f"\nPrice: ${availability.price}"
         responses.append(f"Purchase {availability.version.value}")
 
-    text_str += "\nWould you like to attempt to automatically purchase a console?"
+    text_str += "\n\nWould you like to attempt to automatically purchase a console?"
+    text_str += '\n\nReply with "Yes" or "No"'
 
-    requests.post(
-        settings.TILL_URL,
-        json={
-            "phone": numbers_to_text,
-            "method": "SMS",
-            "questions": [
-                {
-                    "text": text_str,
-                    "tag": "retailer_availability",
-                    "responses": responses + ["Do not purchase"],
-                    "webhook": f"{settings.URL}/purchase-attempt-sms-response",
-                }
-            ],
-        },
+    client.messages.create(
+        body=text_str,
+        from_=settings.TWILIO_PHONE_NUMBER,
+        status_callback=f"{settings.URL}/purchase-attempt-sms-response",
+        to=phone_number,
     )
 
 
