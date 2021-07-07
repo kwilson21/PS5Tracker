6d10fd1a34c7e1cbdfe04e4b5ad8b3b2881bc189
Kazon Wilson <kazonwilson@gmail.com>
2021-07-07 12:35:12 -0500

    refactor web drivers

--- app/services/web_driver.py
+++ app/services/web_driver.py
@@ -5,17 +5,27 @@ from selenium import webdriver
 from app import settings
 
 
-@contextmanager
-def driver_ctx():
+def _get_chrome_driver():
     options = webdriver.ChromeOptions()
     options.binary_location = settings.CHROME_BINARY_LOCATION
     options.headless = True
+    return webdriver.Chrome(settings.CHROME_DRIVER_EXECUTABLE_PATH, chrome_options=options)
 
-    driver = webdriver.Chrome(settings.CHROME_DRIVER_EXECUTABLE_PATH, chrome_options=options)
 
-    # options = webdriver.FirefoxOptions()
-    # options.headless = True
-    # driver = webdriver.Firefox(options=options, service_log_path='/dev/null')
+def _get_firefox_driver():
+    options = webdriver.FirefoxOptions()
+    options.headless = True
+    return webdriver.Firefox(options=options, service_log_path="/dev/null")
+
+
+@contextmanager
+def driver_ctx(use_driver: str = settings.WEB_DRIVER):
+    if use_driver == "chrome":
+        driver = _get_chrome_driver()
+    elif use_driver == "firefox":
+        driver = _get_firefox_driver()
+    else:
+        raise Exception("Invalid driver specified")
 
     try:
         yield driver
